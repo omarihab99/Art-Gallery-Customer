f87923e321a4d042d9364d61a203b0ec7a08e429
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-09-26 22:38:01 +0300

    Deleted unnecessary testing code.

--- product-microservice/src/app.ts
+++ product-microservice/src/app.ts
@@ -4,9 +4,6 @@ import {ProductRouter} from './routes/product.routes';
 const app : Express = express();
 
 app.use(express.json());
-app.get('/', (req: Request, res: Response)=>{
-    res.json('Hello World!');
-});
 app.use('/api/products', ProductRouter);
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
